7bbbba8eee48fa592e6b2a8bd7683accfe5c61bb
Hikazu <149084563+LuthfiSyahputra@users.noreply.github.com>
2023-11-14 23:25:31 +0700

    Initial commit

---
+++ lib/_file.h
@@ -0,0 +1,22 @@
+#ifndef _CUSTOM_FILE_H
+#define _CUSTOM_FILE_H
+
+#include "std_constants.h"
+#include <stdio.h>
+
+
+#if WIN_DEFINED
+#include <windows.h>
+#endif
+
+int _createTempFile(const char* filename);
+
+int _createTempFile(const char* filename) {
+
+    
+
+}
+
+
+
+#endif
---
+++ s.c
@@ -0,0 +1,16 @@
+#include <stdint.h>
+#include <string.h>
+#include <mem.h>
+#include <memory.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <windows.h>
+
+
+int main() {
+    char* str;
+    char* str2;
+    strcasecmp(str, str2);
+    strset(str, 2);
+    
+}
